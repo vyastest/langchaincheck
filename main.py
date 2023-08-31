2263bbf9bbe2a7d7f3cc85cd8dd757098d91025d
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 22:06:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
+
+st.title("new csv app")
