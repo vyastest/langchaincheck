25bb6166a463f2fd474a5ee856d524ff383c55d7
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 22:08:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
+import openai
+openai.api_key = st.secrets["my_api_key"]
 
 st.title("new csv app")
